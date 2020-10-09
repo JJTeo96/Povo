ba66d1d7628ed340a03a1bb1aa89b7c6c3705206
JJTeo96 <teochunkit96@hotmail.com>
2020-10-09 15:02:02 +0800

    Update Banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
 
 
@@ -73,20 +74,22 @@
             <h1 class="text-center font-weight-bold">MOBILE <span style="color: red; ">PLAN</span></h1>
             <h6 class="text-center font-weight-bold">PAVOCOMMS your mobile experiences. Save more as you add more lines <p>for your family, tablets and wearables.</p></h6>
         </div>
-
+       
         <div class="col-12">
-            <div class="row no-gutters">
+            <div class="row no-gutters" style="max-width: 1000px;margin:0 auto" >
                 <div class="col-4 d-flex justify-content-end">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card shadow" style="width: 18rem;">
                         <div class="card img-fluid">
-                            <img class="card-img-top" src="{{asset("img/mainpage/2.png")}}" alt="Card image cap">
+                            <div class="img-gradient"> 
+                                <img class="card-img-top" src="{{asset("img/mainpage/2.png")}}" alt="Card image cap">
+                            </div>
                             <div class="card-img-overlay  d-flex flex-column justify-content-end">
                               <h4 class="card-title" style="color:white;"><b>One Family Plan <br/>for Sharing</b></h4>
                               <p class="card-text" style="color:white">FROM RM90.00/month</p>
-                            </div>
+                            </div>    
                         </div>
                         <div class="card-body">
-                            <h3 class="card-title font-weight-bold">Unlimited Share Plan</h3>
+                            <h3 class="card-title font-weight-bold">Unlimited <p>Share Plan</p></h3>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
                             <hr/>
                             <a class="font-weight-bold" href="#" >View Plan Details</a>
@@ -94,21 +97,37 @@
                     </div>
                 </div>
                 <div class="col-4 d-flex justify-content-center">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="{{asset("img/mainpage/3.png")}}" alt="Card image cap">
+                    <div class="card shadow" style="width: 18rem;">
+                        <div class="card img-fluid ">
+                            <div class="img-gradient"> 
+                                <img class="card-img-top" src="{{asset("img/mainpage/3.png")}}" alt="Card image cap">
+                            </div>
+                            <div class="card-img-overlay  d-flex flex-column justify-content-end">
+                              <h4 class="card-title" style="color:white;"><b>Just Right  <br/>for you</b></h4>
+                              <p class="card-text" style="color:white">FROM RM90.00/month</p>
+                            </div>    
+                        </div>
                         <div class="card-body">
-                            <h3 class="card-title font-weight-bold">PAVO Lite Plan</h3>
+                            <h3 class="card-title font-weight-bold">PAVO Lite <p> Plan</p></h3>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
                             <hr/>
-                            <a class="font-weight-bold " href="#" >View Plan Details</a>
+                            <a class="font-weight-bold" href="#" >View Plan Details</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-4 d-flex justify-content-start">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="{{asset("img/mainpage/4.png")}}" alt="Card image cap">
+                    <div class="card shadow" style="width: 18rem;">
+                        <div class="card img-fluid">
+                            <div class="img-gradient"> 
+                                <img class="card-img-top" src="{{asset("img/mainpage/4.png")}}" alt="Card image cap">
+                            </div>
+                            <div class="card-img-overlay  d-flex flex-column justify-content-end">
+                              <h4 class="card-title" style="color:white;"><b>Always  <br/>Connected</b></h4>
+                              <p class="card-text" style="color:white">FROM RM90.00/month</p>
+                            </div>    
+                        </div>
                         <div class="card-body">
-                            <h3 class="card-title font-weight-bold">PAVO Plus Plan</h3>
+                            <h3 class="card-title font-weight-bold">PAVO Plus <p> Plan</p></h3>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
                             <hr/>
                             <a class="font-weight-bold" href="#" >View Plan Details</a>
@@ -119,11 +138,18 @@
         </div>
 
         <div class="col-12 mt-5">
-            <img src="{{asset("img/mainpage/banner4.png")}}" class="w-100" />
+            <div class="mainpage_img-gradient">
+                <img src="{{asset("img/mainpage/banner4.png")}}" class="w-100" />
+            </div>
+
+            <div class="centered" style="color:white">
+                <h1 class="text-right"><b>Pavocomms is not<br/> just about data, we're<br/> a new lease on life.</b></h1>
+            </div>
+
         </div>
         <div class="col-12 p-5 text-center" style="background-color: #041A39">
             <div class="row no-gutters d-flex justify-content-center">
-                <div class="col-2 text-left text-white p-2">
+                <div class="col-2 text-left text-black p-2">
                     <img src="{{asset("img/pavo_logo2.png")}}" width="100px"/>
                     <br/>
                     Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.

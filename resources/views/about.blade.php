1c01628155dc1111d99f5e88cc78a530e69be30a
albinkoh <mlxhome1997@hotmail.com>
2020-10-12 16:44:31 +0800

    update font color

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,18 +15,21 @@
             </div>
         </div>
         <div class="col-12 p-5">
-            <h1 class="text-center font-weight-bold">Our <span style="color: red; ">Values</span></h1>
-            <h6 class="text-center font-weight-bold">We create meaningful connections by delivering digital products and services to you and your loved <p>ones in order to enrich your life, energise your play, and empower your work!</p></h6>
+            <h1 class="text-center font-weight-bold">
+                <span style="color: #0F2A51;">Our</span>
+                <span style="color: red; ">Values</span>
+            </h1>
+            <h6 class="text-center font-weight-bold" style="color: #0F2A51;">We create meaningful connections by delivering digital products and services to you and your loved <p>ones in order to enrich your life, energise your play, and empower your work!</p></h6>
         </div>
 
         <div class="col-12">
-            <div class="row no-gutters" style="max-width: 1000px; margin: 0 auto;">
-                <div class="col-4 d-flex justify-content-end" style="margin-bottom: 50px">
+            <div class="row no-gutters" style="max-width: 1000px; margin: 0 auto; ">
+                <div class="col-4 d-flex justify-content-end" style="margin-bottom: 50px;">
                     <div class="card border-danger" style="max-width: 18rem;">
-                        <div class="card-body text-danger text-center font-weight-bold" style="font-size: 30px">
+                        <div class="card-body text-danger text-center font-weight-bold" style="font-size: 30px;">
                             Innovation
                             <p class="vl p-1" style="margin-left: 75px; width: 40%; border-bottom: 2px solid red;"></p>
-                                <div class="card-body text-center font-weight-bold p-1" style="color: grey; font-size: 16px">
+                                <div class="card-body text-center font-weight-bold p-1" style="color: #0F2A51; font-size: 16px;">
                                     <p class="card-text">Innovation can enhance our offering to deliver greater value and improve the lives of our customers.</p>
                                 </div>
                         </div>
@@ -37,7 +40,7 @@
                         <div class="card-body text-danger text-center font-weight-bold" style="font-size: 30px">
                             Optimism
                             <p class="vl p-1" style="margin-left: 75px; width: 40%; border-bottom: 2px solid red;"></p>                                          
-                                <div class="card-body text-center font-weight-bold p-1"  style="color: grey; font-size: 16px">    
+                                <div class="card-body text-center font-weight-bold p-1"  style="color: #0F2A51; font-size: 16px">    
                                     <p class="card-text">A little positivity can go a long way in achieving our goals. Optimism makes us hopeful for a better future and invigorates us to conceptualise and realise possibilities.</p>
                                 </div>
                         </div>
@@ -48,7 +51,7 @@
                         <div class="card-body text-danger text-center font-weight-bold" style="font-size: 30px">
                             Trust
                             <p class="vl p-1" style="margin-left: 75px; width: 40%; border-bottom: 2px solid red;"></p>                         
-                                <div class="card-body text-center font-weight-bold p-1"  style="color: grey; font-size: 16px">
+                                <div class="card-body text-center font-weight-bold p-1"  style="color: #0F2A51; font-size: 16px">
                                     <p class="card-text">Earning the trust of our customers and colleagues is vital to our success. We earn this through our honesty, sincere care and genuine acts.</p>
                                 </div>
                         </div>
@@ -58,7 +61,7 @@
         </div>
 
         <div class="col-12 p-5">
-            <h6 class="text-center font-weight-bold" style="margin-top: -40px;margin-bottom 30px">Our values guide us in all that we do, from the way we conduct ourselves to the way we engage with you.</p></h6>
+            <h6 class="text-center font-weight-bold" style="margin-top: -40px;margin-bottom 30px; color: #0F2A51;">Our values guide us in all that we do, from the way we conduct ourselves to the way we engage with you.</p></h6>
         </div>
 
         @include('layout.footer')

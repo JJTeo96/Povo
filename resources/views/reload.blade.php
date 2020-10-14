3180e4aa62918c7fe43c2968ea38dfc5f3d62818
albinkoh <mlxhome1997@hotmail.com>
2020-10-14 22:13:54 +0800

    update radio button

--- resources/views/reload.blade.php
+++ resources/views/reload.blade.php
@@ -12,10 +12,12 @@
     .card-hint{
         border-top:1px solid #EDEDED;
         padding-top:2.5px;
+        font-size: 14px;
     }
     .custom-control-label::before {
         background-color: white;
-        border-color: #ED1B2F;
+        border-color: grey;
+        padding: 10px;
     }
     .card-body{
       padding: 15.56px 15.09px 9.44px 18px;
@@ -32,8 +34,8 @@
         <div class="container">
             <div class="row justify-content-center">
               @include('layout.sidebar')
-                <div class="col-7">
-                    <div class="justify-content-left mb-3" style="color:#ED1B2F;font-size:30px;">
+                <div class="col-7" style="color: #0F2A51;">
+                    <div class="justify-content-left mb-3 font-weight-bolder" style="color:#ED1B2F;font-size:30px;">
                         Reload
                     </div>
                     <div class="row mb-4">
@@ -47,26 +49,38 @@
                                         <label class="custom-control-label" for="customRadio"></label>
                                     </div>
                                 </span>
-                              <p class="card-text mb-3">Validity for 10 days.</p>
-                              <div class="card-hint">free 1 days</div>                           
+                              <p class="card-text mb-3">Validity 10 Days</p>
+                              <div class="card-hint">Free 1 Days</div>                           
                             </div>
                           </div>
                         </div>
                         <div class="col-sm-4">
                             <div class="card card-selected shadow-sm">
                               <div class="card-body">
-                                <span class="card-title"><b>RM10.00</b></span>
-                                <p class="card-text mb-3">Validity for 10 days.</p>
-                                <div class="card-hint">free 1 days</div>                           
+                                <span class="card-title"><b>RM15.00</b></span>
+                                  <span class="float-right">
+                                    <div class="custom-control custom-radio">
+                                        <input type="radio" class="custom-control-input" id="customRadio">
+                                        <label class="custom-control-label" for="customRadio"></label>
+                                    </div>
+                                  </span>
+                                <p class="card-text mb-3">Validity 15 Days</p>
+                                <div class="card-hint">Free 1 Days</div>                           
                               </div>
                             </div>
                           </div>
                           <div class="col-sm-4">
                             <div class="card shadow-sm">
                               <div class="card-body">
-                                <span class="card-title"><b>RM10.00</b></span>
-                                <p class="card-text mb-3">Validity for 10 days.</p>
-                                <div class="card-hint">free 1 days</div>                           
+                                <span class="card-title"><b>RM30.00</b></span>
+                                  <span class="float-right">
+                                    <div class="custom-control custom-radio">
+                                        <input type="radio" class="custom-control-input" id="customRadio">
+                                        <label class="custom-control-label" for="customRadio"></label>
+                                    </div>
+                                  </span>
+                                <p class="card-text mb-3">Validity 30 Days</p>
+                                <div class="card-hint">Free 1 Days</div>                           
                               </div>
                             </div>
                           </div>
@@ -75,18 +89,30 @@
                         <div class="col-sm-4">
                             <div class="card shadow-sm">
                               <div class="card-body">
-                                <span class="card-title"><b>RM10.00</b></span>
-                                <p class="card-text mb-3">Validity for 10 days.</p>
-                                <div class="card-hint">free 1 days</div>                           
+                                <span class="card-title"><b>RM50.00</b></span>
+                                  <span class="float-right">
+                                    <div class="custom-control custom-radio">
+                                        <input type="radio" class="custom-control-input" id="customRadio">
+                                        <label class="custom-control-label" for="customRadio"></label>
+                                    </div>
+                                  </span>
+                                <p class="card-text mb-3">Validity 50 Days</p>
+                                <div class="card-hint">Free 1 Days</div>                           
                               </div>
                             </div>
                           </div>
                           <div class="col-sm-4">
                             <div class="card shadow-sm">
                               <div class="card-body">
-                                <span class="card-title"><b>RM10.00</b></span>
-                                <p class="card-text mb-3">Validity for 10 days.</p>
-                                <div class="card-hint">free 1 days</div>                           
+                                <span class="card-title"><b>RM100.00</b></span>
+                                  <span class="float-right">
+                                    <div class="custom-control custom-radio">
+                                        <input type="radio" class="custom-control-input" id="customRadio">
+                                        <label class="custom-control-label" for="customRadio"></label>
+                                    </div>
+                                  </span>
+                                <p class="card-text mb-3">Validity 120 Days</p>
+                                <div class="card-hint">Free 1 Days</div>                           
                               </div>
                             </div>
                           </div>

351cbd4a6e20632ff59fd76dc55f8b5281074610
albinkoh <mlxhome1997@hotmail.com>
2020-10-16 21:16:11 +0800

    profile done responsive

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -19,46 +19,46 @@
                         <section id="tabs">
                         <nav class="#">
                             <div class="nav nav-tabs nav-fill col-sm-12 col-md-9 col-lg-7" id="nav-tab" role="tablist">
-                                <a class="nav-item nav-link active" id="personal_info_tab" data-toggle="tab" href="#personal_info" role="tab" aria-controls="personal_info" aria-selected="true">Personal Info</a>
-                                <a class="nav-item nav-link" id="password_tab" data-toggle="tab" href="#password" role="tab" aria-controls="password" aria-selected="false">Password</a>
-                                <a class="nav-item nav-link" id="payment_method_tab" data-toggle="tab" href="#payment_method" role="tab" aria-controls="payment_method" aria-selected="false">Payment Method</a>
+                                <a class="nav-item nav-link active" style="text-align:left" id="personal_info_tab" data-toggle="tab" href="#personal_info" role="tab" aria-controls="personal_info" aria-selected="true">Personal Info</a>
+                                <a class="nav-item nav-link" style="text-align:left" id="password_tab" data-toggle="tab" href="#password" role="tab" aria-controls="password" aria-selected="false">Password</a>
+                                <a class="nav-item nav-link" style="text-align:left" id="payment_method_tab" data-toggle="tab" href="#payment_method" role="tab" aria-controls="payment_method" aria-selected="false">Payment Method</a>
                             </div>
                         </nav>
                         </section>
                         <!-- Tab panes -->
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6 col-lg-9 tab-content tabs">
+                        
+                            <div class="tab-content tabs">
                                 <div role="tabpanel" class="tab-pane fade show active" id="personal_info">
-                                    <div class="card shadow" style="width: 799px; margin-top: 20px;">
+                                    <div class="card shadow col-sm-12 col-md-12 col-lg-12" style="margin-top: 20px;">
                                         <form action="#">
-                                            <div class="row d-flex justify-content-left" >
-                                                <div class="col-4" style="margin-left:20px; margin-right: 110px;">
+                                            <div class="row d-flex justify-content-left">
+                                                <div class="col-sm-6 col-md-6 col-lg-6">
                                                     <div style="margin-top: 20px;">
                                                         Full Name
-                                                        <input type="text" id="fullname" class="form-control faq-form" value="Tiana Rosser" style="width: 365.17px;" disabled>
+                                                        <input type="text" id="fullname" class="form-control faq-form" value="Tiana Rosser" disabled>
                                                     </div>
                                                     <div style="margin-top: 20px;">
                                                         ID Type
-                                                        <input type="text" id="id_type" class="form-control faq-form" value="My Kad" style="width: 365.17px;" disabled>
+                                                        <input type="text" id="id_type" class="form-control faq-form" value="My Kad" disabled>
                                                     </div>
                                                     <div style="margin-top: 20px;">
                                                         ID Number
-                                                        <input type="text" id="id_number" class="form-control faq-form" value="9201030181" style="width: 365.17px;" disabled>
+                                                        <input type="text" id="id_number" class="form-control faq-form" value="9201030181" disabled>
                                                     </div>
                                                     <br/>
                                                 </div>
-                                                <div class="col-4" style="margin-left:10px; margin-right: 80px;">
+                                                <div class="col-sm-6 col-md-6 col-lg-6">
                                                     <div style="margin-top: 20px;">
                                                         Email
-                                                        <input type="text" id="fullname" class="form-control faq-form" value="tianarosser@gmail.com" style="width: 365.17px;" disabled>
+                                                        <input type="text" id="fullname" class="form-control faq-form" value="tianarosser@gmail.com" disabled>
                                                     </div>
                                                     <div style="margin-top: 20px;">
                                                         Date Of Birth
-                                                        <input type="text" id="id_type" class="form-control faq-form" value="03 Jan 1992" style="width: 365.17px;" disabled>
+                                                        <input type="text" id="id_type" class="form-control faq-form" value="03 Jan 1992" disabled>
                                                     </div>
                                                     <div style="margin-top: 20px;">
                                                         Gender
-                                                        <input type="text" id="id_number" class="form-control faq-form" value="Placeholders" style="width: 365.17px;" disabled>
+                                                        <input type="text" id="id_number" class="form-control faq-form" value="Placeholders" disabled>
                                                     </div>
                                                     <br/>
                                                 </div>
@@ -69,25 +69,25 @@
                                 <button class="btn btn-radius2 mt-4 cancelBtn" style="background-color:red; color:white; font-size: 12px; width: 136px; padding: 8px; margin-top: -30px; display:none;" type="button">Cancel</button>
                                 </div>
                                 <div role="tabpanel" class="tab-pane fade" id="password">
-                                    <div class="card shadow" style="width: 407px; margin-top: 20px;">
+                                    <div class="card shadow col-sm-6 col-md-6 col-lg-6" style="margin-top: 20px;">
                                         <form action="#">
                                             <div class="row d-flex justify-content-left" >
-                                                <div class="col-4" style="margin-left: 21.21px;">
+                                                <div class="col-sm-12 col-md-12 col-lg-12" >
                                                     <div style="margin-top: 20px;">
                                                         Old Password *
-                                                        <input type="text" id="old_password" class="form-control faq-form"  style="width: 365.17px;">
+                                                        <input type="text" id="old_password" class="form-control faq-form">
                                                     </div>
                                                     <div style="margin-top: 20px;">
                                                         <div style="width: 200px">
                                                             New Password *
                                                         </div>
-                                                        <input type="text" id="new_password" class="form-control faq-form"  style="width: 365.17px;">
+                                                        <input type="text" id="new_password" class="form-control faq-form">
                                                     </div>
                                                     <div style="margin-top: 20px;">
                                                         <div style="width: 200px">
                                                             New Password Again *
                                                         </div>
-                                                        <input type="text" id="password_again" class="form-control faq-form" style="width: 365.17px;">
+                                                        <input type="text" id="password_again" class="form-control faq-form">
                                                     </div>
                                                     <br/>
                                                 </div>
@@ -100,7 +100,7 @@
                                     <div  style="margin-top: 20px;">
                                         <form action="#">
                                             <div class="row d-flex justify-content-left" >
-                                                <div class="col-12" style="#">
+                                                <div class="col-12 col-sm-12 col-md-12 col-lg-12" style="#">
                                                     <div>
                                                         Add your credit / debit card details and save your payment information to your account for future purchases. You can also select a card as a preferred payment or replace your existing card.
                                                     </div>
@@ -111,7 +111,7 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        
                     </div>                      
             </div>
         </div>

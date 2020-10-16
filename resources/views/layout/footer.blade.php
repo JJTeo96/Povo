558a2c4ec6f681829b44817aac9696524d1f12a0
JJTeo96 <teochunkit96@hotmail.com>
2020-10-16 16:26:49 +0800

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -8,20 +8,19 @@
     hr.color {
         border-top: 1px solid #ABABAB;
     }
-
 </style>
 
-{{-- Footer --}}
-<div class="col-12 p-5 text-center" style="background: linear-gradient(rgb(4, 26, 57, 0.95), rgb(4, 26, 57, 0.95)), url({{ asset('img/dxiMe7.png')}});">
+{{-- Footer Web--}}
+<div class="col-12 p-5 text-center d-sm-none d-none d-md-block" style="background: linear-gradient(rgb(4, 26, 57, 0.95), rgb(4, 26, 57, 0.95)), url({{ asset('img/dxiMe7.png')}});">
     <div class="row no-gutters d-flex justify-content-center" style="max-width: 1920px;margin:0 auto">
-        <div class="col-sm-2 text-left text-white p-3" style="margin-right:3%">
+        <div class="col-2 text-left text-white p-3" style="margin-right:3%">
             <img src="{{asset('img/pavo-03.png')}}" height="26px" width="86px" style="margin-bottom: 8px"/>
             <br/>
             <br/>
             <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
         </div>
 
-        <div class="col-sm-2 text-left text-white p-3 ">
+        <div class="col-2 text-left text-white p-3 ">
             <h5 class="footer" style="margin-bottom: 33px">
                 <b>SIDE MAP</b>
             </h5>
@@ -30,7 +29,7 @@
             <a href="#"style="margin-bottom: 10px" class="footer_link">About</a><br/>
         </div>
 
-        <div class="col-sm-2 text-left text-white p-3">
+        <div class="col-2 text-left text-white p-3">
             <h5 style="margin-bottom: 33px">
                 <b>SUPPORT</b>
             </h5>
@@ -38,7 +37,7 @@
             <a href="#" style="margin-bottom: 10px" class="footer_link">Terms & Conditions</a><br/>
             <a href="#" style="margin-bottom: 10px" class="footer_link">Privacy Policy</a><br/>
         </div>
-        <div class="col-sm-2 text-left text-white p-3">
+        <div class="col-2 text-left text-white p-3">
             <h5 style="margin-bottom: 33px">
                 <b>FOLLOW US</b>
             </h5>
@@ -51,4 +50,46 @@
     <hr class="color" />
     <span style="color:#ABABAB">Copyright © 2020 Pavo Communications Sdn. Bhd.(861312-T) All Rights Reserved.</span>
 </div>
-{{-- End Footer --}}
+{{-- End Footer Web --}}
+
+{{-- Footer Mobile--}}
+<div class="col-12 p-5 text-center d-block d-sm-block d-md-none" style="background: linear-gradient(rgb(4, 26, 57, 0.95), rgb(4, 26, 57, 0.95)), url({{ asset('img/dxiMe7.png')}});">
+    <div class="row no-gutters d-flex justify-content-center" style="max-width: 1920px;margin:0 auto">
+        <div class="col-sm-12 col-md-2 text-left text-white p-3 border-bottom" style="margin-right:3%">
+            <img src="{{asset('img/pavo-03.png')}}" height="26px" width="86px" style="margin-bottom: 8px"/>
+            <br/>
+            <br/>
+            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        </div>
+
+        <div class="col-sm-12 col-md-2 text-left text-white p-3 border-bottom">
+            <h5 class="footer" style="margin-bottom: 33px">
+                <b>SIDE MAP</b>
+            </h5>
+            <a href="#" style="margin-bottom: 10px" class="footer_link">Home</a><br/>
+            <a href="#" style="margin-bottom: 10px" class="footer_link">Plans</a><br/>
+            <a href="#"style="margin-bottom: 10px" class="footer_link">About</a><br/>
+        </div>
+
+        <div class="col-sm-12 col-md-2 text-left text-white p-3 border-bottom">
+            <h5 style="margin-bottom: 33px">
+                <b>SUPPORT</b>
+            </h5>
+            <a href="#" style="margin-bottom: 10px" class="footer_link">FAQS</a><br/>
+            <a href="#" style="margin-bottom: 10px" class="footer_link">Terms & Conditions</a><br/>
+            <a href="#" style="margin-bottom: 10px" class="footer_link">Privacy Policy</a><br/>
+        </div>
+        <div class="col-sm-12 col-md-2 text-left text-white p-3 ">
+            <h5 style="margin-bottom: 33px">
+                <b>FOLLOW US</b>
+            </h5>
+            <i class="fab fa-facebook-square"></i>&nbsp; &nbsp;<a href="#" style="margin-bottom: 10px" class="footer_link">Facebook</a><br/>
+            <i class="fab fa-instagram"></i>&nbsp; &nbsp;<a href="#" style="margin-bottom: 10px" class="footer_link">Instagram</a><br/>
+            <i class="fab fa-youtube"></i>&nbsp; &nbsp;<a href="#" style="margin-bottom: 10px" class="footer_link">Youtube</a><br/>
+        </div>
+    </div>
+
+    <hr class="color" />
+    <span style="color:#ABABAB;font-size:12px">Copyright © 2020 Pavo Communications Sdn. Bhd.(861312-T) All Rights Reserved.</span>
+</div>
+{{-- End Footer Mobile --}}

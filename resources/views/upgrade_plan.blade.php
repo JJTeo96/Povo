9d313291b57f48d1d4c2205667058b280174b9b9
JJTeo96 <teochunkit96@hotmail.com>
2020-10-17 17:29:49 +0800

    Update upgrade_plan.blade.php
    add responsive

--- resources/views/upgrade_plan.blade.php
+++ resources/views/upgrade_plan.blade.php
@@ -2,48 +2,58 @@
 @section('title', ' Upgrade Plan')
 @section('content')
 
-<div class="p-5 col-sm-12" >
-    <div class="auto-container row">
-        {{-- Col-3 --}}
-        @include('layout.sidebar')
-        {{-- End Col-3 --}}
+<div class="container-fluid p-0">
+    <div class="pt-5 col-12" >
+        <div class="auto-container row">
+            {{-- Col-3 --}}
+            @include('layout.sidebar')
+            {{-- End Col-3 --}}
 
-        <div class="col-sm-9">
-            <div style="color:#ED1B2F;font-size:33px;font-weight:700;margin-bottom: 31px">
-                Upgrade <span style="color:#0F2A51">Plan</span>
-            </div>
-          
-            <div class="row auto-container" style="min-height: 600px;">
-            <a href="{{ url('/plan_detail') }}" style="text-decoration: none">
-                <div class="card mr-5" style="width: 325px;border:none">
-                    <img class="card-img-top" src="{{asset('img/upgrade_plan/hotlink-prepaid.png')}}" alt="Card image cap">
-  
-                    <div class="card-footer text-muted" style="height: 35px;text-align:center;background-color:#0F2A51;border-radius: 0px 0px 10px 10px">
-                        <div class="auto_container text-white" style="margin-top:-5px;font-size:12px">
-                            View Plan
-                        </div>   
-                    </div>
+            <div class="col-8" style="min-height: 650px">
+                <div class="col p-0 d-block d-sm-block d-md-none">
+                    <p style="color:#0F2A51;font-weight:700"><i class="fas fa-angle-left pr-1"></i>Back to Dashboard</p>
                 </div>
-            </a>
-                    
+                <div class="d-sm-none d-none d-md-block" style="color:#ED1B2F;font-size:33px;font-weight:700;margin-bottom: 31px">
+                    Upgrade <span style="color:#0F2A51">Plan</span>
+                </div>
+            
+                <div class="row auto-container">
+                    <div class="col-lg-5 col-md-12 col-12 p-0 mb-3">
+                        <a href="{{ url('/plan_detail') }}" style="text-decoration: none">
+                        <div class="card " style="width: 325px;border:none">
+                            <img class="card-img-top" src="{{asset('img/upgrade_plan/hotlink-prepaid.png')}}" alt="Card image cap">
+        
+                            <div class="card-footer text-muted" style="height: 35px;text-align:center;background-color:#0F2A51;border-radius: 0px 0px 10px 10px">
+                                <div class="auto_container text-white" style="margin-top:-5px;font-size:12px">
+                                    View Plan
+                                </div>   
+                            </div>
+                        </div>
+                        </a>
+                    </div>
 
-            <a href="#" style="text-decoration: none">
-                <div class="card" style="width: 325px;border:none">
-                    <img class="card-img-top" src="{{asset('img/upgrade_plan/switch-to-hotlink-rebrand-herobanner-desktop.png')}}" alt="Card image cap">
-  
-                    <div class="card-footer text-muted" style="height: 35px;text-align:center;background-color:#0F2A51;border-radius: 0px 0px 10px 10px">
-                        <div class="auto_container text-white" style="margin-top:-5px;font-size:12px">
-                            View Plan
-                        </div>   
+                    <div class="col-lg-4 col-md-12 col-12 p-0">
+                        <a href="#" style="text-decoration: none">
+                            <div class="card" style="width: 325px;border:none">
+                                <img class="card-img-top" src="{{asset('img/upgrade_plan/switch-to-hotlink-rebrand-herobanner-desktop.png')}}" alt="Card image cap">
+            
+                                <div class="card-footer text-muted" style="height: 35px;text-align:center;background-color:#0F2A51;border-radius: 0px 0px 10px 10px">
+                                    <div class="auto_container text-white" style="margin-top:-5px;font-size:12px">
+                                        View Plan
+                                    </div>   
+                                </div>
+                            </div>
+                        </a>
                     </div>
+                
+                        
+
+                
                 </div>
-            </a>
+                
             </div>
-               
         </div>
     </div>
 </div>
 
-
-@include('layout.footer')
 @endsection

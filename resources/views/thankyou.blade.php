874fa1712f03fe89b0413145ee53eca0ad7396d9
JJTeo96 <teochunkit96@hotmail.com>
2020-10-17 19:36:41 +0800

    Update thank you

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,7 +1,7 @@
-@extends('layout.app')
-@section('title', ' Thank You')
+@extends('layout.app_payment')
+@section('title', ' Success')
 @section('content')
-        <div class="container mt-5">
+        <div class="container mt-5 pt-3" style="min-height: 700px">
             <div class="col-12 col-md-12 ">
             <div class="row justify-content-center">
               <div class="shadow success_circle rounded-circle d-flex align-items-center justify-content-center ">
@@ -11,14 +11,14 @@
               </div>
             </div>
             <div class="row justify-content-center">
-                <p style="font: normal normal medium 30px/41px SF UI Display"><b>Thank you</b></p>
+                <p style="font-size:30px;font-weight:700;color:#151522"><b>Thank you</b></p>
             </div>
             <div class="row justify-content-center">
                 <div class="col-12 col-md-6">
-                    <p class="text-wrap text-center">Your request is being processed now. You will receive a confirmation SMS on your purchase is successful.</p>
+                    <p class="text-wrap text-center font-weight-bold">Your request is being processed now. You will receive a confirmation SMS on your purchase is successful.</p>
                 </div>
             </div>
-            <div class="row justify-content-center">
+            <div class="row justify-content-center mt-3">
                 <a class="#" href="{{ url('/dashboard') }}">
                     <button type="button" class="btn back_button btn-lg btn-light">Back to Dashboard</button>
                 </a>

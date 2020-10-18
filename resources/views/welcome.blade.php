e984296ec82ab3ec368684fd29408984fc6b0e7b
JJTeo96 <teochunkit96@hotmail.com>
2020-10-18 12:49:28 +0800

    Update welcome.blade.php
    update carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
 @section('content')
 <div class="container-fluid p-0">
     {{-- Carousel --}}
-    <div class="col-12 p-0">
+    {{-- <div class="col-12 p-0">
         <div id="carouselExampleIndicators" class="carousel slide col-lg-12 col-md-12 col-sm-6 p-0" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -53,7 +53,72 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
+    </div> --}}
+
+    <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="6000">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
+        </ol>
+        
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                    <!-- 
+                    If you need more browser support use https://scottjehl.github.io/picturefill/
+                    If a picture looks blurry on a retina device you can add a high resolution like this
+                    <source srcset="img/blog-post-1000x600-2.jpg, blog-post-1000x600-2@2x.jpg 2x" media="(min-width: 768px)">
+
+                    What image sizes should you use? This can help - https://codepen.io/JacobLett/pen/NjramL
+                     -->
+                <picture>
+                    <source srcset="{{asset('img/mainpage/banner3.png')}}" media="(min-width: 1400px)">
+                    <source srcset="https://dummyimage.com/1400x500/#007aeb/4196e5" media="(min-width: 769px)">
+                    <source srcset="https://dummyimage.com/800x500/007aeb/4196e5" media="(min-width: 577px)">
+                    <img srcset="https://dummyimage.com/600x500/007aeb/4196e5" alt="responsive image" class="d-block img-fluid">
+                </picture>
+
+                {{-- <div class="carousel-caption">
+                    <div>
+                        <h2>Digital Craftsmanship</h2>
+                        <p>We meticously build each site to get results</p>
+                        <span class="btn btn-sm btn-outline-secondary">Learn More</span>
+                    </div>
+                </div> --}}
+            </div>
+
+            <!-- /.carousel-item -->
+            <div class="carousel-item">
+                <picture>
+                    <source srcset="{{asset('img/mainpage/banner4.png')}}" media="(min-width: 1400px)">
+                    <source srcset="https://dummyimage.com/1400x500/007aeb/4196e5" media="(min-width: 769px)">
+                    <source srcset="https://dummyimage.com/800x500/007aeb/4196e5" media="(min-width: 577px)">
+                    <img srcset="https://dummyimage.com/600x500/007aeb/4196e5" alt="responsive image" class="d-block img-fluid">
+                </picture>
+            </div>
+
+            <!-- /.carousel-item -->
+            <div class="carousel-item">
+                <picture>
+                    <source srcset="{{asset('img/banner5.png')}}" media="(min-width: 1400px)">
+                    <source srcset="https://dummyimage.com/1400x500/007aeb/4196e5" media="(min-width: 769px)">
+                    <source srcset="https://dummyimage.com/800x500/007aeb/4196e5" media="(min-width: 577px)">
+                    <img srcset="https://dummyimage.com/600x500/007aeb/4196e5" alt="responsive image" class="d-block img-fluid">
+                </picture>
+            </div>
+        </div>
+        
+        <!-- /.carousel-inner -->
+        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
+    <!-- /.carousel -->
     {{-- End Carousel --}}
     
     {{-- Mobile Plan --}}
@@ -223,7 +288,7 @@
 
     {{-- Image Bottom Mobile--}}
     <div class="col-12 p-0 d-block d-sm-block d-md-none " style="position: relative;">
-        <div class="w-100" style="display: flex; align-items: center; min-height: 597px; background-position: center; background-attachment: scroll; background-size: cover; background-repeat: no-repeat; background-image: url({{ asset('img/mainpage/Group-249.jpg')}});">
+        <div class="w-100" style="display: flex; align-items: center; min-height: 597px; background-position: center; background-attachment: scroll; background-size: cover; background-repeat: no-repeat; background-image: url({{ asset('img/mainpage/banner4_edited.png')}});">
             <div class="col-12  d-flex">
                 <div class="col-12" style="text-align: center">
                     <div class="col-sm-6 col-md-6 col-lg-12" style="color:white; font-size: 26px;text-shadow: 0px 3px 20px #00000094;text-aligin:right;font-weight:700;margin-top:400px">

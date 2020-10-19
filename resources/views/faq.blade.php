9b70560c9abc190a575501fecda5df71d90a410c
JJTeo96 <teochunkit96@hotmail.com>
2020-10-19 16:33:24 +0800

    Update faq.blade.php
    update accordion

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -19,12 +19,74 @@
     box-sizing: content-box;
     background-color: transparent;
 }
+form .form-control:focus{
+  /* border-color: #ced4da; */
+  box-shadow: none;
+}
 .row-bg{
         background-attachment:initial;
         background-position:top center;
         background-repeat: no-repeat;
         background-size:cover;
     }
+
+.accordion .card {
+	background: none;
+	border: none;
+}
+.accordion .card .card-header {
+	background: none;
+	border: none;
+	padding: .4rem 1rem;
+	font-family: "Roboto", sans-serif;
+}
+.accordion .card-header h2 span {
+	float: left;
+	margin-top: 10px;
+}
+.accordion .card-header .btn {
+	color: #2f2f31;
+	font-size: 1.04rem;
+	text-align: left;
+	position: relative;
+	font-weight: 500;
+	padding-left: 2rem;
+}
+.accordion .card-header i {
+	font-size: 1.2rem;
+	font-weight: bold;
+	position: absolute;
+	left: 0;
+	top: 9px;
+}
+.accordion .card-header .btn:hover {
+	color: red;
+}
+.accordion .card-body {
+	color: #324353;
+	padding: 0.5rem 3rem;
+}
+.page-title {
+	margin: 3rem 0 3rem 1rem;
+	font-family: "Roboto", sans-serif;
+	position: relative;
+}
+.page-title::after {
+	content: "";
+	width: 80px;
+	position: absolute;
+	height: 3px;
+	border-radius: 1px;
+	background: #73bb2b;
+	left: 0;
+	bottom: -15px;
+}
+.accordion .highlight .btn {
+	color: #74bd30;
+}
+.accordion .highlight i {
+	transform: rotate(180deg);
+}
 </style>
 
 <div class="col-12 p-5">
@@ -34,89 +96,58 @@
     </h1>
 </div>
 
-<div class="col-12 p-5" id="accordion" style="margin-top: -70px; max-width: 1000px; margin: 0 auto; color: #0F2A51;">
-    <div class="card shadow" style="margin-bottom: 5px;">
-      <div class="card-header" id="headingOne">
-        <h5 class="mb-0">
-          <button class="btn btn-link collapsed font-weight-bold text-danger firstBtn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            <img class="redBtn" src="https://img.icons8.com/small/25/000000/ED1B2F/circled-chevron-up.png"/>&nbsp;
-            {{-- <img class="blueBtn" src="https://img.icons8.com/small/25/000000/circled-chevron-down.png"/>&nbsp; --}}
-            How do I see my Pavocomms billing history, weekly or monthly invoice?
-          </button>
-        </h5>
-      </div>
-  
-      <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-        <div class="card-body">
-            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </div>
-      </div>
-    </div>
-    <div class="card shadow" style="margin-bottom: 5px;">
-      <div class="card-header" id="headingTwo">
-        <h5 class="mb-0">
-          <button class="btn btn-link collapsed font-weight-bold" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            <img src="https://img.icons8.com/small/25/000000/circled-chevron-down.png"/>&nbsp;
-            <span style="color: #0F2A51;">Can I pay for Pavocomms with cash?</span>
-          </button>
-        </h5>
-      </div>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-        <div class="card-body">
-          Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        
-        </div>
-      </div>
-    </div>
-    <div class="card shadow" style="margin-bottom: 5px;">
-      <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-          <button class="btn btn-link collapsed font-weight-bold" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            <img src="https://img.icons8.com/small/25/000000/circled-chevron-down.png"/>&nbsp;
-            <span style="color: #0F2A51;">Why isn't my payment method accepted?</span>
-          </button>
-        </h5>
-      </div>
-      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </div>
-      </div>
-    </div>
-
-    <div class="card shadow" style="margin-bottom: 5px;">
-        <div class="card-header" id="headingFour">
-          <h5 class="mb-0">
-            <button class="btn btn-link collapsed font-weight-bold" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                <img src="https://img.icons8.com/small/25/000000/circled-chevron-down.png"/>&nbsp;
-                <span style="color: #0F2A51;">Why was I charged earlier than excepted?</span>
-            </button>
-          </h5>
-        </div>
-        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
-          <div class="card-body">
-              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+<div class="col-12 pt-5 pr-0 pl-0 " id="accordion" style="margin-top: -70px; max-width: 1000px; margin: 0 auto; color: #0F2A51;">
+{{-- Accordion --}}
+  <div class="container-lg p-0">
+    <div class="row no-gutters">
+      <div class="col-lg-12 pr-0 pl-0">
+        <div class="accordion" id="accordionExample">
+          <div class="card shadow pb-2 mb-2" >
+            <div class="card-header" id="headingOne">
+              <h2 class="clearfix mb-0">
+                <a class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><i class="fa fa-chevron-circle-down"></i>  How do I see my Pavocomms billing history, weekly or monthly invoice?</a>									
+              </h2>
+            </div>
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div class="card-body">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            </div>
+          </div>
+          <div class="card shadow pb-2 mb-2">
+            <div class="card-header" id="headingTwo">
+              <h2 class="mb-0">
+                <a class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><i class="fa fa-chevron-circle-down"></i> Can I pay for Pavocomms with cash?</a>
+              </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+              <div class="card-body">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            </div>
+          </div>
+          <div class="card shadow pb-2 mb-2">
+            <div class="card-header" id="headingThree">
+              <h2 class="mb-0">
+                <a class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"><i class="fa fa-chevron-circle-down"></i> Why isn't my payment method accepted?</a>                     
+              </h2>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+              <div class="card-body"> Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            </div>
+          </div>
+          <div class="card shadow pb-2 mb-5 ">
+            <div class="card-header" id="headingFour">
+              <h2 class="mb-0">
+                <a class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour"><i class="fa fa-chevron-circle-down"></i> How do I cancel Pavocomms Subscription?</a>                               
+              </h2>
+            </div>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+              <div class="card-body">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="card shadow">
-        <div class="card-header" id="headingFive">
-          <h5 class="mb-0">
-            <button class="btn btn-link collapsed font-weight-bold" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                <img src="https://img.icons8.com/small/25/000000/circled-chevron-down.png"/>&nbsp;
-                <span style="color: #0F2A51;">How do I cancel Pavocomms Subscription?</span>
-            </button>
-          </h5>
-        </div>
-        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
-          <div class="card-body">
-              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </div>
-        </div>
-      </div>
+    </div>
   </div>
-
-
+</div>
+{{-- End Accordion --}}
     <div class="col-12 p-5 text-center row-bg" style="background-image: linear-gradient(rgb(4, 26, 57, 0.95), rgb(4, 26, 57, 0.95)), url({{ asset('img/faq/contact-bg.png')}})">
       <div class="container">  
         <div class="d-flex justify-content-center mt-4">
@@ -203,3 +234,18 @@
         });
     </script>
 @endpush --}}
+
+<script>
+  $(document).ready(function(){
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.show").each(function(){
+      $(this).prev(".card-header").addClass("highlight");
+    });
+    
+    // Highlight open collapsed element 
+    $(".card-header .btn").click(function(){
+      $(".card-header").not($(this).parents()).removeClass("highlight");
+      $(this).parents(".card-header").toggleClass("highlight");
+    });
+  });
+</script>
